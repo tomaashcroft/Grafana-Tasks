7c9146e127b5a2b9b45a9da0f3988e5fa70986d7
Tom Ashcroft <tomaashcroft@outlook.com>
2015-08-24 10:26:44 +0100

    Update getstats-io.py

--- getstats-io.py
+++ getstats-io.py
@@ -1,26 +1,20 @@
 #!/usr/bin/env python
 # This script is looking for data within different lustre stats.
 
+#importing the right libraries
 import glob
+#Looking for the files within this directory and outputing certain lines from it
 for filename in glob.glob("/proc/diskstats"):
-		#Looking for the files within this directory and outputing certain lines from it
+		#whilst we have the file open as read only and assigned to "procfile"
 		with open(filename,'r') as procfile:
-			ioname = filename.split(" ")[-2]
+			#new variable splits the file name by a space.
 			with open('/tmp/testgetio.txt', 'a') as iostats:
-				for line procfile:
+				for line in procfile:
 					fieldentry = line.split()
-					if 'read_bytes' in line:
-						lusstats.write(ostname + " current read_calls are " + fieldentry[1]+"\n")
-						lusstats.write(ostname + " current read_bytes are " + fieldentry[6]+"\n")
-						lusstats.write("\n")
-					elif 'write_bytes' in line:
-						lusstats.write(ostname + " current write_calls are " + fieldentry[1]+"\n")
-						lusstats.write(ostname + " current write_bytes are " + fieldentry[6]+"\n")
-						lusstats.write("\n")
-					elif 'cache_access' in line:
-						print(line)
-					elif 'cache_hit' in line:
-						print(line)
-					elif 'cache_miss' in line:
-						print(line)
-			
+					ioname = fieldentry[2]
+					if 'sd' in line:
+						iostats.write(ioname + " readn is " + fieldentry[3]+"\n")
+						iostats.write(ioname + " readt is " + fieldentry[6]+"\n")
+						iostats.write(ioname + " writen is " + fieldentry[7]+"\n")
+						iostats.write(ioname + " writet is " + fieldentry[10]+"\n")
+						iostats.write("\n")
