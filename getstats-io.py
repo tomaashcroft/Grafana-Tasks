2d7dc01baf82819e233d6f2a621a66e4c0e42c64
Tom Ashcroft <tomaashcroft@outlook.com>
2015-08-24 10:34:02 +0100

    Update getstats-io.py

--- getstats-io.py
+++ getstats-io.py
@@ -10,11 +10,15 @@ for filename in glob.glob("/proc/diskstats"):
 			#new variable splits the file name by a space.
 			with open('/tmp/testgetio.txt', 'a') as iostats:
 				for line in procfile:
+					#this is splitting the line and assigning a new variable
 					fieldentry = line.split()
+					#a new variable to split to extract the 3rd field from a line.
 					ioname = fieldentry[2]
 					if 'sd' in line:
+						#this is writing to file, the name of the "sd line" plus the required fields
 						iostats.write(ioname + " readn is " + fieldentry[3]+"\n")
 						iostats.write(ioname + " readt is " + fieldentry[6]+"\n")
 						iostats.write(ioname + " writen is " + fieldentry[7]+"\n")
 						iostats.write(ioname + " writet is " + fieldentry[10]+"\n")
 						iostats.write("\n")
+					
